6a24a09820bc6deee2e7bbe754c37bc407f86bf7
pasharyan <pasharyan50@gmail.com>
2026-05-04 11:52:53 +0700

    masukkan class

--- constructorberparameter.cpp
+++ constructorberparameter.cpp
@@ -1,3 +1,14 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+class mahasiswa
+{
+private:
+    int nim;
+    string nama;
+
+public:
+    // Constructor dengan parameter
+    mahasiswa(int nim, const string &nama);
+};
