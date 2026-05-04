878441b98cd600477f3fb65d9da85b6f5ebec8ec
pasharyan <pasharyan50@gmail.com>
2026-05-04 11:53:13 +0700

    masukkan mahasiswa

--- constructorberparameter.cpp
+++ constructorberparameter.cpp
@@ -12,3 +12,14 @@ public:
     // Constructor dengan parameter
     mahasiswa(int nim, const string &nama);
 };
+
+// Definisi constructor
+mahasiswa::mahasiswa(int nim, const string &nama)
+{
+    this->nim = nim;
+    this->nama = nama;
+
+    cout << "Constructor Dengan Parameter Terpanggil" << endl;
+    cout << "NIM  : " << this->nim << endl;
+    cout << "Nama : " << this->nama << endl;
+}
