f199e4208eefe602f900904e1bc2dbaa36f968fb
pasharyan <pasharyan50@gmail.com>
2026-05-04 11:36:31 +0700

    masukkan data mahasiswa

--- constructoroverloading.cpp
+++ constructoroverloading.cpp
@@ -15,3 +15,23 @@ public:
     mahasiswa(int iNim, string iNama);
     void cetak();
 };
+
+mahasiswa ::mahasiswa()
+{
+}
+
+mahasiswa ::mahasiswa(int iNim)
+{
+    nim = iNim; // definisi hanya NIM
+}
+
+mahasiswa ::mahasiswa(string iNama)
+{
+    nama = iNama; // definisi hanya NAMA
+}
+
+mahasiswa ::mahasiswa(int iNim, string iNama)
+{
+    nim = iNim; // definisi NIM dan NAMA
+    nama = iNama;
+}
